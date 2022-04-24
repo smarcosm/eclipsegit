1834a530ec37245061194cd00ecdb52c9ca0397c
Sebastião Marcos <sebastiaomarcos49@gmail.com.br>
2022-04-24 13:28:17 -0300

    Criando banco de dados em memória

---
+++ aws-image-upload/src/main/java/com/smarcosm/awsimageupload/database/FakeUserProfileDataStore.java
@@ -0,0 +1,24 @@
+package com.smarcosm.awsimageupload.database;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Repository;
+import com.smarcosm.awsimageupload.profile.UserProfile;
+
+@Repository
+public class FakeUserProfileDataStore {
+	
+	private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
+	
+	static {
+		USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "Isadora", null));
+		USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "Isadora", null));
+		
+	}
+
+	public List<UserProfile> getUserProfiles() {
+		return USER_PROFILES;
+	}
+}

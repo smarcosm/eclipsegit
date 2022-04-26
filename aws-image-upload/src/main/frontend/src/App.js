c5ba3fe77fa808c967193da1cc0139ac56fdf529
SMarcosM <sebastiaomarcos49@gmail.com>
2022-04-26 18:51:06 -0300

    Axios e components

--- aws-image-upload/src/main/frontend/src/App.js
+++ aws-image-upload/src/main/frontend/src/App.js
@@ -1,23 +1,27 @@
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+const UserProfiles = () => {
+
+  const fetchUserProfiles = () => {
+    axios.get("http://localhost:8080/api/v1/user-profile").then(res => {
+      console.log(res)
+    });
+  }
+
+  useEffect(() => {
+    fetchUserProfiles()
+  }, []);
+
+  return <h1>Olá</h1>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserProfiles />
     </div>
   );
 }

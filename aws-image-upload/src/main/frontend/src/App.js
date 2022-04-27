d7492563a51169bb8b4b83b1b18cda33339f2500
SMarcosM <sebastiaomarcos49@gmail.com>
2022-04-27 16:59:09 -0300

    React Dropzone

--- aws-image-upload/src/main/frontend/src/App.js
+++ aws-image-upload/src/main/frontend/src/App.js
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import { useDropzone } from 'react-dropzone';
 
 const UserProfiles = () => {
 
@@ -21,12 +22,38 @@ const UserProfiles = () => {
   return userProfiles.map((userProfile, index) => {
     return (
       <div key={index}>
+        {/* imagem de perfil */}
+        <br />
+        <br />
         <h1>{userProfile.username}</h1>
         <p>{userProfile.userProfileId}</p>
+        <Dropzone />
+        <br />
       </div>
     )
-  })
+  });
+};
 
+function Dropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Faça algo com os arquivos
+    const file = acceptedFiles[0];
+    console.log(file);
+    
+
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
+    </div>
+  )
 }
 function App() {
   return (
